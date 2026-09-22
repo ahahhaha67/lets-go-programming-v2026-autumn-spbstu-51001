9cb0f45d8aa2000531d997501afab8fe46eddcdd
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-09-22 22:09:46 +0300

    [TASK-1] fix for tests

--- ivan.krivoshapov/task-1/cmd/service/main.go
+++ ivan.krivoshapov/task-1/cmd/service/main.go
@@ -5,19 +5,19 @@ import "fmt"
 func main() {
 
 	var firstArg int64
-	_, err := fmt.Scan(&firstArg)
+	_, err := fmt.Scanln(&firstArg)
 	if err != nil {
 		fmt.Println("error reading first argument")
 		return
 	}
 	var secondArg int64
-	_, err = fmt.Scan(&secondArg)
+	_, err = fmt.Scanln(&secondArg)
 	if err != nil {
 		fmt.Println("error reading second argument")
 		return
 	}
 	var operator string
-	_, err = fmt.Scan(&operator)
+	_, err = fmt.Scanln(&operator)
 	if err != nil {
 		fmt.Println("error reading operator")
 		return
@@ -25,11 +25,11 @@ func main() {
 
 	switch operator {
 	case "+":
-		fmt.Print(firstArg + secondArg)
+		fmt.Println(firstArg + secondArg)
 	case "-":
-		fmt.Print(firstArg - secondArg)
+		fmt.Println(firstArg - secondArg)
 	case "*":
-		fmt.Print(firstArg * secondArg)
+		fmt.Println(firstArg * secondArg)
 	case "/":
 		if secondArg == 0 {
 			fmt.Println("can't divide by zero")
