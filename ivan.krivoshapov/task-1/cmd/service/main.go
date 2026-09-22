072fbaa655e2b5e87495cd8928baa6c2948a5b3f
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-09-22 22:01:25 +0300

    [TASK-1] add errors detection

--- ivan.krivoshapov/task-1/cmd/service/main.go
+++ ivan.krivoshapov/task-1/cmd/service/main.go
@@ -4,24 +4,37 @@ import "fmt"
 
 func main() {
 
-	var firtArg int64
+	var firstArg int64
+	_, err := fmt.Scan(&firstArg)
+	if err != nil {
+		fmt.Println("error reading first argument")
+		return
+	}
 	var secondArg int64
+	_, err = fmt.Scan(&secondArg)
+	if err != nil {
+		fmt.Println("error reading second argument")
+		return
+	}
 	var operator string
-
-	fmt.Scan(&firtArg, &secondArg, &operator)
+	_, err = fmt.Scan(&operator)
+	if err != nil {
+		fmt.Println("error reading operator")
+		return
+	}
 
 	switch operator {
 	case "+":
-		fmt.Print(firtArg + secondArg)
+		fmt.Print(firstArg + secondArg)
 	case "-":
-		fmt.Print(firtArg - secondArg)
+		fmt.Print(firstArg - secondArg)
 	case "*":
-		fmt.Print(firtArg * secondArg)
+		fmt.Print(firstArg * secondArg)
 	case "/":
 		if secondArg == 0 {
 			fmt.Println("can't divide by zero")
 		} else {
-			fmt.Println(firtArg / secondArg)
+			fmt.Println(firstArg / secondArg)
 		}
 	default:
 		fmt.Println("unknown operator")
