e4810cbf4daf8c3bff4432556337e8be6d2722a6
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-09-22 21:55:45 +0300

    [TASK-1] simple calculator

--- ivan.krivoshapov/task-1/cmd/service/main.go
+++ ivan.krivoshapov/task-1/cmd/service/main.go
@@ -1,5 +1,29 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	var firtArg int64
+	var secondArg int64
+	var operator string
+
+	fmt.Scan(&firtArg, &secondArg, &operator)
+
+	switch operator {
+	case "+":
+		fmt.Print(firtArg + secondArg)
+	case "-":
+		fmt.Print(firtArg - secondArg)
+	case "*":
+		fmt.Print(firtArg * secondArg)
+	case "/":
+		if secondArg == 0 {
+			fmt.Println("can't divide by zero")
+		} else {
+			fmt.Println(firtArg / secondArg)
+		}
+	default:
+		fmt.Println("unknown operator")
+	}
 }
